e93f2113467791a5c084443e855d3c676d21c259
Máté Menyhárt <mateka.mateka07@gmail.com>
2025-02-10 17:41:06 +0100

    Mova date to seperate variable

--- set.menu.php
+++ set.menu.php
@@ -3,8 +3,13 @@ require_once "config.php";
 authUser();
 $mysql = new mysqli($dbcred["host"], $dbcred["username"], $dbcred["password"], $dbcred["db"]);
 $mysql->query("SET NAMES utf8");
+$nextdatestmt = $mysql->prepare("SELECT `date` FROM `menu` LEFT JOIN `deadlines` ON `menu`.`date` BETWEEN `deadlines`.`from` AND `deadlines`.`to` WHERE `date` > ? AND ((CURDATE() BETWEEN `start` AND `end`) OR `start` IS NULL) LIMIT 1");
+if (isset($_GET["date"]))
+{
+    $date = $_GET["date"];
+}
 $stmt = $mysql->prepare("SELECT CASE WHEN CURDATE() BETWEEN `start` AND `end` THEN TRUE ELSE FALSE END AS `fillable` FROM `deadlines` WHERE ? BETWEEN `from` AND `to`");
-$stmt->execute([$_GET["date"]]);
+$stmt->execute([$date]);
 $allowedtofill = $stmt->get_result()->fetch_row();
 if ($allowedtofill !== null) {
     $allowedtofill = (bool)$allowedtofill[0];
@@ -18,9 +23,8 @@ if (isset($_POST["date"]) && $allowedtofill) {
     if ($stmt->execute()) {
         Message::addMessage("Választás sikeresen mentve!", MessageType::success);
     }
-    $stmt = $mysql->prepare("SELECT `date` FROM `menu` LEFT JOIN `deadlines` ON `menu`.`date` BETWEEN `deadlines`.`from` AND `deadlines`.`to` WHERE `date` > ? AND ((CURDATE() BETWEEN `start` AND `end`) OR `start` IS NULL) LIMIT 1");
-    $stmt->execute([$_POST["date"]]);
-    $row = $stmt->get_result()->fetch_row();
+    $nextdatestmt->execute([$_POST["date"]]);
+    $row = $nextdatestmt->get_result()->fetch_row();
     if ($row !== null) {
         header("Location: set.menu.php?date=".$row[0]);
         exit;
@@ -32,12 +36,12 @@ if ($allowedtofill === false) {
     Message::addMessage("A kitöltési határidő erre a napra lejárt!", MessageType::danger);
 }
 $stmt = $mysql->prepare("SELECT `menuId` FROM `choices` WHERE `date` = ? AND `userId` = ?");
-$stmt->bind_param("si", $_GET["date"], $_SESSION["userId"]);
+$stmt->bind_param("si", $date, $_SESSION["userId"]);
 $stmt->execute();
 $choice = $stmt->get_result()->fetch_row();
 if ($choice !== null) $choice = $choice[0];
 $stmt = $mysql->prepare("SELECT `id`, `description` FROM `menu` WHERE `date` = ?");
-$stmt->execute([$_GET["date"]]);
+$stmt->execute([$date]);
 $menu = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-echo $twig->render("set.menu.html.twig", ["date" => $_GET["date"] ?? null, "id" => $choice, "menu" => $menu ?? null, "fillable" => $allowedtofill]);
+echo $twig->render("set.menu.html.twig", ["date" => $date, "id" => $choice, "menu" => $menu ?? null, "fillable" => $allowedtofill]);
 ?>
