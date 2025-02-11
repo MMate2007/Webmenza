fa453070af418ffe039266587305acdae5ed886e
Máté Menyhárt <mateka.mateka07@gmail.com>
2025-02-11 18:38:03 +0100

    Display today's choice on user homepage

--- home.php
+++ home.php
@@ -1,5 +1,11 @@
 <?php
 require_once "config.php";
 authUser();
-echo $twig->render("home.html.twig");
+$mysql = new mysqli($dbcred["host"], $dbcred["username"], $dbcred["password"], $dbcred["db"]);
+$mysql->query("SET NAMES utf8");
+$stmt = $mysql->prepare("SELECT `description` FROM `menu` INNER JOIN `choices` ON `menu`.`date` = `choices`.`date` AND `menu`.`id` = `choices`.`menuId` WHERE `choices`.`date` = CURRENT_DATE AND `choices`.`userId` = ?");
+$stmt->bind_param("i", $_SESSION["userId"]);
+$stmt->execute();
+$todayMenu = $stmt->get_result()->fetch_row();
+echo $twig->render("home.html.twig", ["todayMenu" => $todayMenu]);
 ?>
