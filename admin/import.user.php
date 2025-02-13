49daa6be9b11b79f1e4944f1bc58f4969ff8dba6
Máté Menyhárt <mateka.mateka07@gmail.com>
2025-02-13 18:34:30 +0100

    Fix timeout issue and use transactions

--- admin/import.user.php
+++ admin/import.user.php
@@ -1,4 +1,5 @@
 <?php
+set_time_limit(0);
 require_once "../config.php";
 authUser(1);
 if (isset($_FILES["spreadsheet"])) {
@@ -11,12 +12,17 @@ if (isset($_FILES["spreadsheet"])) {
     while ($row = $result->fetch_array()) {
         $groups[$row[0]] = $row[1];
     }
+    $mysql->begin_transaction();
+    try {
     $userstmt = $mysql->prepare("INSERT INTO `users`(`name`, `password`, `groupId`) VALUES (?,?,?)");
     $userstmt->bind_param("ssi", $name, $password, $id);
+    $groupstmt = $mysql->prepare("INSERT INTO `groups`(`name`) VALUES (?)");
+    $groupstmt->bind_param("s", $groupname);
     foreach ($rowGenerator as $row) {
         $id = array_search($row[1], $groups);
         if (!$id) {
-            $stmt = $mysql->prepare("INSERT INTO `groups`(`name`) VALUES (?)")->execute([$row[1]]);
+            $groupname = $row[1];
+            $groupstmt->execute();
             $id = $mysql->insert_id;
             $groups[$id] = $row[1];
         }
@@ -24,7 +30,12 @@ if (isset($_FILES["spreadsheet"])) {
         $password = password_hash($row[2], PASSWORD_DEFAULT);
         $userstmt->execute();
     }
+    $mysql->commit();
     Message::addMessage("Felhasználók importálása sikeres!", MessageType::success);
+    } catch (mysqli_sql_exception $e) {
+        $mysql->rollback();
+        throw $e;
+    }
 }
 echo $twig->render("import.user.html.twig");
 ?>
