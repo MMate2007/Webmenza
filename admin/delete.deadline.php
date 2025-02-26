56a82944bb90c39da0d79cef786adb949f50e253
Máté Menyhárt <mateka.mateka07@gmail.com>
2025-02-26 18:01:14 +0100

    Fix not deleting deadline if there are no modifications

--- admin/delete.deadline.php
+++ admin/delete.deadline.php
@@ -6,7 +6,7 @@ $mysql->query("SET NAMES utf8");
 $stmt = $mysql->prepare("UPDATE `choices` INNER JOIN `modifications` ON `choices`.`date` = `modifications`.`date` AND `choices`.`userId` = `modifications`.`userId` SET `choices`.`menuId` = `modifications`.`value` WHERE (`modifications`.`approved` != 1 OR `modifications`.`approved` IS NULL) AND `modifications`.`date` BETWEEN (SELECT `from` FROM `deadlines` WHERE `id` = ?) AND (SELECT `to` FROM `deadlines` WHERE `id` = ?)");
 $stmt->bind_param("ii", $_GET["id"], $_GET["id"]);
 $stmt->execute();
-$stmt = $mysql->prepare("DELETE `deadlines`, `modifications` FROM `deadlines` INNER JOIN `modifications` ON `modifications`.`date` BETWEEN `deadlines`.`from` AND `deadlines`.`to` WHERE `deadlines`.`id` = ?");
+$stmt = $mysql->prepare("DELETE `deadlines`, `modifications` FROM `deadlines` LEFT JOIN `modifications` ON `modifications`.`date` BETWEEN `deadlines`.`from` AND `deadlines`.`to` WHERE `deadlines`.`id` = ?");
 $stmt->bind_param("i", $_GET["id"]);
 $stmt->execute();
 Message::addMessage("A kapcsolódó módosítási kérelmek elfogadásra kerültek!", MessageType::warning);
