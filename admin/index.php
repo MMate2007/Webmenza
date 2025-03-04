8b4e071c7957e16922a45a02d4cbafa33b6f3cf6
Máté Menyhárt <mateka.mateka07@gmail.com>
2025-03-04 18:13:48 +0100

    Show stats and basic links on adminindex

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,9 @@
 <?php
-
 require_once "../config.php";
-
 authUser(1);
-
-echo $twig->render("adminbase.html.twig");
-
+$mysql = new mysqli($dbcred["host"], $dbcred["username"], $dbcred["password"], $dbcred["db"]);
+$mysql->query("SET NAMES utf8");
+$nonreg = $mysql->query("SELECT COUNT(*) FROM `users` WHERE `registered` = 0")->fetch_row()[0];
+$allreg = $mysql->query("SELECT COUNT(*) FROM `users`")->fetch_row()[0];
+echo $twig->render("adminhome.html.twig", ["nonreg" => $nonreg, "allreg" => $allreg]);
 ?>
