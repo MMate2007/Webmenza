6b6e70475658200650a8da39909a47e66e6903a6
Máté Menyhárt <mateka.mateka07@gmail.com>
2025-03-04 18:18:42 +0100

    Show passkey and notification stats

--- admin/index.php
+++ admin/index.php
@@ -5,5 +5,7 @@ $mysql = new mysqli($dbcred["host"], $dbcred["username"], $dbcred["password"], $
 $mysql->query("SET NAMES utf8");
 $nonreg = $mysql->query("SELECT COUNT(*) FROM `users` WHERE `registered` = 0")->fetch_row()[0];
 $allreg = $mysql->query("SELECT COUNT(*) FROM `users`")->fetch_row()[0];
-echo $twig->render("adminhome.html.twig", ["nonreg" => $nonreg, "allreg" => $allreg]);
+$passkeys = $mysql->query("SELECT COUNT(*) FROM `passkeys`")->fetch_row()[0];
+$notis = $mysql->query("SELECT COUNT(*) FROM `notificationsubscriptions`")->fetch_row()[0];
+echo $twig->render("adminhome.html.twig", ["nonreg" => $nonreg, "allreg" => $allreg, "passkeys" => $passkeys, "notis" => $notis]);
 ?>
