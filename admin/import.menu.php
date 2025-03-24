f68e77016800a7fac70777eaabd5cd1d04e9c074
Máté Menyhárt <mateka.mateka07@gmail.com>
2025-03-24 18:21:53 +0100

    Import with mealtype

--- admin/import.menu.php
+++ admin/import.menu.php
@@ -1,12 +1,12 @@
 <?php
 require_once "../config.php";
 authUser(1);
+$mysql = new mysqli($dbcred["host"], $dbcred["username"], $dbcred["password"], $dbcred["db"]);
+$mysql->query("SET NAMES utf8");
 if (isset($_FILES["spreadsheet"])) {
     $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($_FILES["spreadsheet"]["tmp_name"]);
     $sheet = $spreadsheet->getSheet(0);
     $rowGenerator = $sheet->rangeToArrayYieldRows("A2:".$sheet->getHighestDataColumn()."2");
-    $mysql = new mysqli($dbcred["host"], $dbcred["username"], $dbcred["password"], $dbcred["db"]);
-    $mysql->query("SET NAMES utf8");
     foreach ($rowGenerator as $row) {
         $astart = array_search("A menü", $row);
         $bstart = array_search("B menü", $row);
@@ -14,8 +14,8 @@ if (isset($_FILES["spreadsheet"])) {
     $rowGenerator = $sheet->rangeToArrayYieldRows("A3:".$sheet->getHighestDataColumn().$sheet->getHighestDataRow());
     $mysql->begin_transaction();
     try {
-    $stmt = $mysql->prepare("INSERT INTO `menu`(`date`, `id`, `description`) VALUES (?,?,?)");
-    $stmt->bind_param("sis", $date, $id, $description);
+    $stmt = $mysql->prepare("INSERT INTO `menu`(`date`, `mealId`, `id`, `description`) VALUES (?,?,?,?)");
+    $stmt->bind_param("siis", $date, $_POST["meal"], $id, $description);
     foreach ($rowGenerator as $row) {
         if ($row[$astart] != null && $row[$astart+1] != null)
 {        $date = date_format(date_create($row[0]), "Y-m-d");
@@ -35,5 +35,6 @@ if (isset($_FILES["spreadsheet"])) {
         throw $e;
     }
 }
-echo $twig->render("import.menu.html.twig");
+$meals = $mysql->query("SELECT `id`, `name` FROM `meals`")->fetch_all(MYSQLI_ASSOC);
+echo $twig->render("import.menu.html.twig", ["meals" => $meals]);
 ?>
