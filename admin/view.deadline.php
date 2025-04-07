22b6e5210e142e2d0b1a3fe754fbc683aedb9a4f
Máté Menyhárt <mateka.mateka07@gmail.com>
2025-04-07 18:06:50 +0200

    Show deadline's statistic correctly in new system

--- admin/view.deadline.php
+++ admin/view.deadline.php
@@ -11,10 +11,18 @@ $stmt = $mysql->prepare("SELECT COUNT(DISTINCT `date`) FROM `menu` INNER JOIN `d
 $stmt->bind_param("i", $_GET["id"]);
 $stmt->execute();
 $days = $stmt->get_result()->fetch_row()[0];
-$users = $mysql->query("SELECT COUNT(*) FROM `users` LEFT JOIN `groups` ON `groupId` = `groups`.`id` WHERE `registered` = 1 AND `groups`.`name` NOT LIKE '\_%'")->fetch_row()[0];
+$users = $mysql->query("SELECT `users`.`id` FROM `users` LEFT JOIN `groups` ON `groupId` = `groups`.`id` WHERE `registered` = 1 AND `groups`.`name` NOT LIKE '\_%'");
+$possiblechoicecounterstmt = $mysql->prepare("SELECT COUNT(DISTINCT `date`, `mealId`) FROM `menu` INNER JOIN `deadlines` ON `deadlines`.`id` = ? AND `date` BETWEEN `from` AND `to` WHERE `mealId` NOT IN (SELECT `mealId` FROM `excludegroupsfrommeals` INNER JOIN `users` ON `users`.`groupId` = `excludegroupsfrommeals`.`groupId` AND `users`.`id` = ?)");
+$possiblechoicecounterstmt->bind_param("ii", $_GET["id"], $userId);
+$possiblechoicecounter = 0;
+while ($row = $users->fetch_array()) {
+    $userId = $row["id"];
+    $possiblechoicecounterstmt->execute();
+    $possiblechoicecounter += $possiblechoicecounterstmt->get_result()->fetch_row()[0];
+}
 $stmt = $mysql->prepare("SELECT COUNT(*) FROM `choices` INNER JOIN `deadlines` ON `date` BETWEEN `from` AND `to` WHERE `id` = ?");
 $stmt->bind_param("i", $_GET["id"]);
 $stmt->execute();
 $choices = $stmt->get_result()->fetch_row()[0];
-echo $twig->render("view.deadline.html.twig", ["from" => $data[0], "to" => $data[1], "start" => $data[2], "end" => $data[3], "days" => $days, "users" => $users, "choices" => $choices]);
+echo $twig->render("view.deadline.html.twig", ["from" => $data[0], "to" => $data[1], "start" => $data[2], "end" => $data[3], "choices" => $choices, "possible" => $possiblechoicecounter, "days" => $days]);
 ?>
