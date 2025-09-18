29a6ab46e5451667c165b1d71cc6c935036d7d93
Máté Menyhárt <mateka.mateka07@gmail.com>
2025-09-18 20:05:45 +0200

    Deadline statistics of groups

--- admin/view.deadline.php
+++ admin/view.deadline.php
@@ -16,5 +16,20 @@ $stmt = $mysql->prepare("SELECT COUNT(*) FROM `choices` INNER JOIN `deadlines` O
 $stmt->bind_param("i", $_GET["id"]);
 $stmt->execute();
 $choices = $stmt->get_result()->fetch_row()[0];
-echo $twig->render("view.deadline.html.twig", ["from" => $data[0], "to" => $data[1], "start" => $data[2], "end" => $data[3], "days" => $days, "users" => $users, "choices" => $choices]);
+$stmt = $mysql->prepare("SELECT `groups`.`name` AS `groupName`, `groups`.`id` AS `groupId`, COUNT(`choices`.`date`) AS `choicesCount` FROM `choices` RIGHT JOIN `users` ON `users`.`id` = `choices`.`userId` RIGHT JOIN `groups` ON `groups`.`id` = `users`.`groupId` WHERE ((`date` BETWEEN (SELECT `from` FROM `deadlines` WHERE `deadlines`.`id` = ?) AND (SELECT `to` FROM `deadlines` WHERE `deadlines`.`id` = ?)) OR `date` IS NULL) AND `registered` = 1 AND `groups`.`name` NOT LIKE '\_%' GROUP BY `groups`.`id` ORDER BY `choicesCount`, `groups`.`name`");
+$stmt->bind_param("ii", $_GET["id"], $_GET["id"]);
+$stmt->execute();
+$groupchoicesres = $stmt->get_result();
+$groupmemberscountres = $mysql->query("SELECT `groupId`, COUNT(`id`) AS `count` FROM `users` GROUP BY `groupId`");
+while ($row = mysqli_fetch_array($groupmemberscountres)) {
+    $groupmemberscount[$row["groupId"]] = $row["count"];
+}
+while ($row = $groupchoicesres->fetch_array()) {
+    $groupchoices[] = [
+        "groupName" => $row["groupName"],
+        "choicesCount" => $row["choicesCount"],
+        "groupMembersCount" => $groupmemberscount[$row["groupId"]]
+    ];
+}
+echo $twig->render("view.deadline.html.twig", ["from" => $data[0], "to" => $data[1], "start" => $data[2], "end" => $data[3], "days" => $days, "users" => $users, "choices" => $choices, "groupchoices" => $groupchoices]);
 ?>
