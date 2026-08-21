60cb4acc4e5b1b4400ea316d2706c8936143877c
Máté Menyhárt <mateka.mateka07@gmail.com>
2026-08-21 15:52:09 +0200

    Added diet field to add.user and edit.user forms

--- admin/add.user.php
+++ admin/add.user.php
@@ -9,17 +9,21 @@ $result = $mysql->query("SELECT * FROM `groups`");
 while ($row = $result->fetch_assoc()) {
     $groups[] = $row;
 }
+$result = $mysql->query("SELECT * FROM `diets`");
+while ($row = $result->fetch_assoc()) {
+    $diets[] = $row;
+}
 if (isset($_POST["name"])) {
     if ($_POST["password"] != $_POST["password2"]) {
         $_SESSION["messages"][] = new Message("A két jelszó nem egyezik!", MessageType::danger);
-        echo $twig->render("adduser.html.twig", ["groups" => $groups]);
+        echo $twig->render("adduser.html.twig", ["groups" => $groups ?? null, "diets" => $diets ?? null]);
         exit;
     }
-    $statement = $mysql->prepare("INSERT INTO `users`(`name`, `password`, `groupId`, `registered`) VALUES (?,?,?,?)");
+    $statement = $mysql->prepare("INSERT INTO `users`(`name`, `password`, `groupId`, `registered`, `dietId`) VALUES (?,?,?,?,?)");
     $group = ($_POST["group"] == "") ? null : (int)$_POST["group"];
     $registered = $_POST["registered"] ?? 0;
     $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
-    $statement->bind_param("ssii", $_POST["name"], $password, $group, $registered);
+    $statement->bind_param("ssiii", $_POST["name"], $password, $group, $registered, $_POST["diet"]);
     try {
     $statement->execute();
     } catch (mysqli_sql_exception $e) {
@@ -33,5 +37,5 @@ if (isset($_POST["name"])) {
         Message::addMessage("Felhasználó sikeresen létrehozva!", MessageType::success);
     }
 }
-echo $twig->render("add.user.html.twig", ["groups" => $groups]);
+echo $twig->render("add.user.html.twig", ["groups" => $groups ?? null, "diets" => $diets ?? null]);
 ?>
--- admin/edit.user.php
+++ admin/edit.user.php
@@ -17,17 +17,18 @@ if (isset($_POST["password"]) && isset($_POST["password2"])) {
     }
 }
 if (isset($_POST["name"])) {
-    $stmt = $mysql->prepare("UPDATE `users` SET `name`=?,`groupId`=?,`registered`=? WHERE `id` = ?");
+    $stmt = $mysql->prepare("UPDATE `users` SET `name`=?,`groupId`=?,`registered`=?,`dietId`=? WHERE `id` = ?");
     $group = ($_POST["group"] == "") ? null : (int)$_POST["group"];
     $registered = $_POST["registered"] ?? 0;
-    $stmt->bind_param("siii", $_POST["name"], $group, $registered, $_GET["id"]);
+    $stmt->bind_param("siiii", $_POST["name"], $group, $registered, $_POST["diet"], $_GET["id"]);
     $stmt->execute();
     Message::addMessage("Sikeres mentés!", MessageType::success);
 }
-$stmt = $mysql->prepare("SELECT `users`.`name`, `groupId`, `registered` FROM `users` WHERE `users`.`id` = ?");
+$stmt = $mysql->prepare("SELECT `users`.`name`, `groupId`, `registered`, `dietId` FROM `users` WHERE `users`.`id` = ?");
 $stmt->bind_param("i", $_GET["id"]);
 $stmt->execute();
 $user = $stmt->get_result()->fetch_row();
 $groups = $mysql->query("SELECT * FROM `groups`")->fetch_all(MYSQLI_ASSOC);
-echo $twig->render("edit.user.html.twig", ["user" => $user, "groups" => $groups]);
+$diets = $mysql->query("SELECT * FROM `diets`")->fetch_all(MYSQLI_ASSOC);
+echo $twig->render("edit.user.html.twig", ["user" => $user, "groups" => $groups, "diets" => $diets]);
 ?>
