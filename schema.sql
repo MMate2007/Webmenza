5fa628b060d53531b825fb9b88ccaa77393c6d6f
Máté Menyhárt <mateka.mateka07@gmail.com>
2026-08-21 15:43:22 +0200

    Added diets and menudiets table, added users.dietId with default to (1, Normál)

--- schema.sql
+++ schema.sql
@@ -4,23 +4,39 @@ CREATE TABLE `groups` (
     PRIMARY KEY (`id`),
     UNIQUE KEY (`name`)
 );
+CREATE TABLE `diets` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY(`id`),
+    UNIQUE (`name`)
+);
+INSERT INTO `diets` (`id`, `name`) VALUES (1, 'Normál');
 CREATE TABLE `users` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `groupId` INT UNSIGNED NULL DEFAULT NULL,
     `registered` BOOLEAN NOT NULL DEFAULT FALSE,
+    `dietId` INT UNSIGNED NOT NULL DEFAULT 1,
     `autochoice` JSON NULL DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY (`name`, `groupId`),
-    FOREIGN KEY (`groupId`) REFERENCES `groups`(`id`) ON DELETE
-    SET NULL ON UPDATE CASCADE
+    FOREIGN KEY (`groupId`) REFERENCES `groups`(`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (`dietId`) REFERENCES `diets`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 CREATE TABLE `menu` (
     `date` DATE NOT NULL,
     `id` INT UNSIGNED NOT NULL,
     `description` TEXT NOT NULL,
     PRIMARY KEY (`date`, `id`)
+);
+CREATE TABLE `menudiets` (
+    `menuDate` DATE NOT NULL,
+    `menuId` INT UNSIGNED NOT NULL,
+    `dietId` INT UNSIGNED NOT NULL,
+    FOREIGN KEY (`dietId`) REFERENCES `diets`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (`menuDate`, `menuId`) REFERENCES `menu`(`date`, `id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (`menuDate`, `menuId`, `dietId`)
 );
 CREATE TABLE `choices` (
     `userId` INT UNSIGNED NOT NULL,
