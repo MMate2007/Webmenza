cbfa248eb1949414c04a87e7eb55e2ff3315b310
Máté Menyhárt <mateka.mateka07@gmail.com>
2026-08-26 11:00:32 +0200

    Import diet with users

--- admin/import.user.php
+++ admin/import.user.php
@@ -5,29 +5,46 @@ authUser(1);
 if (isset($_FILES["spreadsheet"])) {
     $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($_FILES["spreadsheet"]["tmp_name"]);
     $sheet = $spreadsheet->getSheet(0);
-    $rowGenerator = $sheet->rangeToArrayYieldRows("A2:C".$sheet->getHighestDataRow());
+    $rowGenerator = $sheet->rangeToArrayYieldRows("A2:D".$sheet->getHighestDataRow());
     $mysql = new mysqli($dbcred["host"], $dbcred["username"], $dbcred["password"], $dbcred["db"]);
     $mysql->query("SET NAMES utf8");
     $result = $mysql->query("SELECT `id`, `name` FROM `groups`");
     while ($row = $result->fetch_array()) {
         $groups[$row[0]] = $row[1];
     }
+    $result = $mysql->query("SELECT `id`, `name` FROM `diets`");
+    while ($row = $result->fetch_array()) {
+        $diets[$row[0]] = $row[1];
+    }
     $mysql->begin_transaction();
     try {
-    $userstmt = $mysql->prepare("INSERT INTO `users`(`name`, `password`, `groupId`) VALUES (?,?,?)");
-    $userstmt->bind_param("ssi", $name, $password, $id);
+    $userstmt = $mysql->prepare("INSERT INTO `users`(`name`, `password`, `groupId`, `dietId`) VALUES (?,?,?,?)");
+    $userstmt->bind_param("ssii", $name, $password, $id, $dietid);
     $groupstmt = $mysql->prepare("INSERT INTO `groups`(`name`) VALUES (?)");
     $groupstmt->bind_param("s", $groupname);
+    $dietstmt = $mysql->prepare("INSERT INTO `diets`(`name`) VALUES (?)");
+    $dietstmt->bind_param("s", $dietname);
     foreach ($rowGenerator as $row) {
         $id = array_search($row[1], $groups);
         if (!$id) {
             $groupname = $row[1];
+            if ($groupname === null) {
+                $id = null;
+            } else {
             if (!preg_match("/^[^\/\[\]\?\*\\']*$/", $groupname)) {
                 throw new InvalidArgumentException("Group name cannot contain [ ] / ? * \ ' characters!");
             }
             $groupstmt->execute();
             $id = $mysql->insert_id;
             $groups[$id] = $row[1];
+            }
+        }
+        $dietid = array_search($row[3], $diets);
+        if (!$dietid) {
+            $dietname = $row[3];
+            $dietstmt->execute();
+            $dietid = $mysql->insert_id;
+            $diets[$dietid] = $row[3];
         }
         $name = $row[0];
         $password = password_hash($row[2], PASSWORD_DEFAULT);
