e0bf77a504c9befc47e464beccdcd1ec2a182a77
Máté Menyhárt <mateka.mateka07@gmail.com>
2026-09-10 15:26:27 +0200

    Added diet attribute to add.menu

--- admin/add.menu.php
+++ admin/add.menu.php
@@ -4,6 +4,7 @@ authUser(1);
 $mysql = new mysqli($dbcred["host"], $dbcred["username"], $dbcred["password"], $dbcred["db"]);
 $mysql->query("SET NAMES utf8");
 if (isset($_POST["date"])) {
+if (isset($_POST["diets"])) {
     $menuidstmt = $mysql->prepare("SELECT MAX(`id`) FROM `menu` WHERE `date` = ?");
     $menuidstmt->execute([$_POST["date"]]);
     $result = $menuidstmt->get_result();
@@ -11,8 +12,16 @@ if (isset($_POST["date"])) {
     $query = $mysql->prepare("INSERT INTO `menu`(`date`, `id`, `description`) VALUES (?,?,?)");
     $query->bind_param("sis", $_POST["date"], $menuid, $_POST["menu"]);
     if ($query->execute()) {
+        $stmt = $mysql->prepare("INSERT INTO `menudiets`(`menuDate`, `menuId`, `dietId`) VALUES (?,?,?)");
+        $stmt->bind_param("sii", $_POST["date"], $menuid, $dietid);
+        foreach ($_POST["diets"] as $dietid) {
+            $stmt->execute();
+        }
         Message::addMessage("Menü létrehozása sikeres!", MessageType::success);
     }
-}
-echo $twig->render("add.menu.html.twig");
+}else{
+    Message::addMessage("Legalább egy étrendet ki kell jelölni!", MessageType::danger);
+}}
+$diets = $mysql->query("SELECT * FROM `diets`")->fetch_all(MYSQLI_ASSOC);
+echo $twig->render("add.menu.html.twig", ["diets" => $diets]);
 ?>
