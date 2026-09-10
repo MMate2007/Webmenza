09479809df07fc31937ba27d003d0beec2a253dc
Máté Menyhárt <mateka.mateka07@gmail.com>
2026-09-10 15:26:38 +0200

    Show diet attribute on list.menu

--- admin/list.menu.php
+++ admin/list.menu.php
@@ -11,7 +11,7 @@ while ($row = $result->fetch_array()) {
 $menu = [];
 if ($dates != null) {
     foreach ($dates as $date) {
-        $stmt = $mysql->prepare("SELECT `id`, `description` FROM `menu` WHERE `date` = ?");
+        $stmt = $mysql->prepare("SELECT `menu`.`id`, `description`, GROUP_CONCAT(`diets`.`name` SEPARATOR ', ') AS `diet` FROM `menu` INNER JOIN `menudiets` ON `menudiets`.`menuDate` = `menu`.`date` AND `menudiets`.`menuId` = `menu`.`id` INNER JOIN `diets` ON `menudiets`.`dietId` = `diets`.`id` WHERE `date` = ? GROUP BY `menu`.`date`, `menu`.`id`, `menu`.`description`");
         $stmt->execute([$date]);
         $menu[$date] = $stmt->get_result()->fetch_all();
     }
